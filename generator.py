4d10c4d8f4c6cfc8f01b37eca2aa35116fe25371
rosen902 <corentin.ehlinger@gmail.com>
2022-07-05 19:17:41 +0200

    Version MacOSX

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
-from cgitb import html
 import webbrowser
 import os
+import sys
 from tkinter import *
 
 def generate_html(surname, name, job, email):
@@ -19,15 +19,21 @@ def generate_html(surname, name, job, email):
 </html>
     '''
     html_content = content.format(**locals())
-    outfile = open('.ts_signature.html','w')
-    outfile.write(html_content)
+    bundle_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
+    path_to_signature = os.path.abspath(os.path.join(bundle_dir,'.ts_signature.html'))
+    with open(path_to_signature, "w") as outfile:
+        outfile.write(html_content)
     outfile.close()
 
 
 def preview(surname, name, job, email):
     generate_html(surname, name, job, email)
-    webbrowser.open('.ts_signature.html')
-    os.remove('.ts_signature.html')
+    bundle_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
+    path_to_signature = os.path.abspath(os.path.join(bundle_dir,'.ts_signature.html'))
+    path_to_signature = "file://" + path_to_signature
+    webbrowser.open_new(path_to_signature)
+    
+
 
 def show_code(surname, name, job, email):
     generate_html(surname, name, job, email)
@@ -35,10 +41,25 @@ def show_code(surname, name, job, email):
     window.title('TinoSign - Code')
     window.configure(background='white')
     window.geometry("600x400")
-    f = open('.ts_signature.html','r')
-    data = f.read()
-    os.remove('.ts_signature.html')
-    result = Text(window)
+    window.resizable(0,0)
+
+    bundle_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
+    path_to_signature = os.path.abspath(os.path.join(bundle_dir,'.ts_signature.html'))
+    with open(path_to_signature, "r") as outfile:
+        data = outfile.read()
+
+    outfile.close()
+    result = Text(window,width=101,height=101)
     result.insert(0.0,data)
+    result.config(state=DISABLED)
     result.pack()
     window.mainloop()
+
+def google_help():
+    webbrowser.open_new('https://support.google.com/mail/answer/8395?hl=fr&co=GENIE.Platform%3DDesktop')
+
+def yahoo_help():
+    webbrowser.open_new('https://fr.aide.yahoo.com/kb/SLN28060.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAANoMO01EImZvj5dOwGvBBV_xIulXq0BmWZ1AwbafLFvpUf_9KjNt-TuKqFgUOBj7cC8l65A8_f13Z62dWeFiRqHMQ42KoRqJ4_sXAKsyb-HK_JeNBIqsKvg2F5qO7GxHJhA--wLhYXSa9ZZyJWfvMd6EFHXxHf1rqtbC_SIbYvwQ')
+
+def outlook_help():
+    webbrowser.open_new('https://support.microsoft.com/fr-fr/office/créer-une-signature-de-courrier-31fb24f9-e698-4789-b92a-f0e777f774ca')
--- main.py
+++ main.py
@@ -1,53 +1,72 @@
-from tkinter import *
+from tkinter import Tk, Label, Entry, ttk
+from turtle import bgcolor
+from tkmacosx import Button
 from generator import *
 
-# Création de la fenetre
+
+# Création de la fenetre et du fichier de base
 window = Tk()
 window.title('TinoSign')
-window.configure(background='white')
-window.geometry("500x300")
+window.geometry("600x300")
 window.resizable(0,0)
+generate_html("unknown","unknown","unknown","unknown")
+
+#Instance de styles
+style = ttk.Style(window)
+style.theme_use('aqua')
+
+# Création du menu
+menubar = Menu(window)
+menuhelp = Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Aide',menu=menuhelp)
+menuhelp.add_command(label="Ajouter à Gmail", command=google_help)
+menuhelp.add_command(label="Ajouter à Outlook", command=outlook_help)
+menuhelp.add_command(label="Ajouter à Yahoo !", command=yahoo_help)
+
+
+window.config(menu=menubar)
+
 
 # Création et insertion du formulaire
-main_frame = Frame(window,background='white')
+main_frame = Frame(window)
 main_frame.grid_columnconfigure((0,1),weight=1)
 
 # Création des labels
-surname_label = Label(main_frame, text="Prénom : ",background="white")
-name_label = Label(main_frame, text="Nom : ",background="white")
-job_label = Label(main_frame, text="Poste : ",background="white")
-email_label = Label(main_frame, text="Email : ",background="white")
+surname_label = ttk.Label(main_frame, text="Prénom : ")
+name_label = ttk.Label(main_frame, text="Nom : ")
+job_label = ttk.Label(main_frame, text="Poste : ")
+email_label = ttk.Label(main_frame, text="Email : ")
 
 # Création des entrées associées
-surname_entry = Entry(main_frame,width=50)
-name_entry = Entry(main_frame,width=50)
-job_entry = Entry(main_frame,width=50)
-email_entry = Entry(main_frame,width=50)
+surname_entry = ttk.Entry(main_frame,width=50,background='white')
+name_entry = ttk.Entry(main_frame,width=50,background='white')
+job_entry = ttk.Entry(main_frame,width=50,background='white')
+email_entry = ttk.Entry(main_frame,width=50,background='white')
 
 # Organisation du formulaire
-surname_label.grid(row=3,column=0,pady=4)
-surname_entry.grid(row=3,column=1,pady=4)
+surname_label.grid(row=3,column=0,pady=10)
+surname_entry.grid(row=3,column=1,pady=10)
 
-name_label.grid(row=4,column=0,pady=4)
-name_entry.grid(row=4,column=1,pady=4,padx=4)
+name_label.grid(row=4,column=0,pady=10)
+name_entry.grid(row=4,column=1,pady=10,padx=10)
 
-job_label.grid(row=5,column=0,pady=4)
-job_entry.grid(row=5,column=1,pady=4,padx=4)
+job_label.grid(row=5,column=0,pady=10)
+job_entry.grid(row=5,column=1,pady=10,padx=10)
 
-email_label.grid(row=6,column=0,pady=4)
-email_entry.grid(row=6,column=1,pady=4,padx=4)
+email_label.grid(row=6,column=0,pady=10)
+email_entry.grid(row=6,column=1,pady=10,padx=10)
 
 main_frame.pack()
 
-secondary_frame = Frame(window,background='white',width=100,pady=10)
+secondary_frame = Frame(window,width=100,pady=10)
 
 # Création du bouton d'apeçu
-preview_button = Button(secondary_frame,text="Aperçu dans le navigateur",command=lambda: preview(surname_entry.get(),name_entry.get(),job_entry.get(),email_entry.get()),background='#4575b9',activebackground='cyan',foreground='white')
-preview_button.pack(side=LEFT)
+preview_button = ttk.Button(secondary_frame, text='Aperçu dans le navigateur', padding=10 , command=lambda: preview(surname_entry.get(),name_entry.get(),job_entry.get(),email_entry.get()))
+preview_button.pack(side=LEFT,padx=10)
 
 # Création du bouton de génération du code
-result_button = Button(secondary_frame,text="Générer signature",command=lambda: show_code(surname_entry.get(),name_entry.get(),job_entry.get(),email_entry.get()),background='#4575b9',activebackground='cyan',foreground='white')
-result_button.pack(side=RIGHT)
+result_button = ttk.Button(secondary_frame,text="Générer signature", padding=10,command=lambda: show_code(surname_entry.get(),name_entry.get(),job_entry.get(),email_entry.get()))
+result_button.pack(side=RIGHT,padx=10)
 secondary_frame.pack()
 
 window.mainloop()
